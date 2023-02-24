c0a8eb246751566bc2eec99b28b7dc425c5b9c86
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-02-24 13:39:38 +0100

    Add bonus

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <script>
 // import componente
 import AppTitle from "./components/AppTitle.vue";
+import AppHero from "./components/AppHero.vue";
 
 //definizione componente
 export default {
   components: {
     AppTitle,
+    AppHero,
   },
 };
 </script>
 
 <template>
   <AppTitle />
+  <AppHero />
 </template>
 
 <style scoped></style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,17 @@
+<script>
+// codice js
+</script>
+
+<template>
+  <!-- HTML -->
+  <section class="jumbotron">
+    <div>Ciao sono un Jumbotron (o quasi)</div>
+  </section>
+</template>
+
+<style scoped>
+.jumbotron {
+  background-color: brown;
+  height: 400px;
+}
+</style>
